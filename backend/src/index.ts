b1c36304bb60d02c49b1605f4a6633e7c2bdcb9b
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-05-30 22:33:47 +0530

    created routes for auth and messages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import authRoutes from "./routes/auth.route"
+import messageRoutes from "./routes/message.route"
 
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello world!")
-})
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 app.listen(port, () => {
     console.log("Server is running on port 5000");

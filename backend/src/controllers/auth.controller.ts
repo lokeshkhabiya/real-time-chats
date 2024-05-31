b792a23aaf8d2713d295f3792307b438d8055409
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-06-01 00:15:43 +0530

    login/logout auth route complete

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -52,6 +52,38 @@ export const signup = async (req: Request, res: Response) => {
     }
 }
 
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.findUnique({where: {username}});
+        
+        if (!user) return res.status(400).json({error: "Invalid credentials."});
 
-export const login = async (req: Request, res: Response) => {}
-export const logout = async (req: Request, res: Response) => {}
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+        if (!isPasswordCorrect) return res.status(400).json({error: "Invalid credentials."});
+
+        generateToken(user.id, res);
+        
+        res.status(201).json({
+            id: user.id,
+            fullname: user.fullname,
+            username: user.username,
+            profilePic: user.profilePic,
+        })
+
+    } catch (error: any) {
+        console.log("Error while login controller.", error.message);
+        res.status(500).json({error: "Internal server error."})
+    }
+}
+
+export const logout = async (req: Request, res: Response) => {
+    try {
+        res.cookie("jwt", "", {maxAge: 0})
+        res.status(201).json({ message: "Logged out successfully."})
+    } catch (error: any) {
+        console.log("Error while logout controller.", error.message);
+        res.status(500).json({error: "Internal server error."})
+    }
+}
